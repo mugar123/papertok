ce8f359786cd1b10c65ed9dd3beb6fd571da9f5a
Nicolás Muñoz García <nicolasmunozgarcia@MacBook-Air-de-Nicolas.local>
2026-06-14 21:10:21 +0200

    🚀 Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/papertok/',
   plugins: [react()],
   server: {
     proxy: {
